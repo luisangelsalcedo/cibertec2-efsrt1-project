01bdc9b330cdf92cf5b19b1f361b34c70d45d030
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-04-15 22:20:39 -0500

    feat: Center window and set program title

--- src/gui/Home.java
+++ src/gui/Home.java
@@ -53,7 +53,8 @@ public class Home extends JFrame implements ActionListener {
 	 */
 	public Home() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setSize(450, 300);
+		setLocationRelativeTo(null);
 		
 		// set menu Items
 		mntmSalir = new JMenuItem("Salir");
