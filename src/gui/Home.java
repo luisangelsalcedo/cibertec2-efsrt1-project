95bb842963f168a2fd4410adc92f5ac4b8e63f44
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-04-15 21:46:57 -0500

    feat: Add Action events

--- src/gui/Home.java
+++ src/gui/Home.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -8,8 +10,9 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
-public class Home extends JFrame {
+public class Home extends JFrame implements ActionListener {
 
 
 	private static final long serialVersionUID = 1L;
@@ -54,13 +57,23 @@ public class Home extends JFrame {
 		
 		// set menu Items
 		mntmSalir = new JMenuItem("Salir");
-		mntmConsultar = new JMenuItem("Consultar");
-		mntmModificar = new JMenuItem("Modificar");
-		mntmListar = new JMenuItem("Listar");
+		mntmConsultar = new JMenuItem("Consultar producto");
+		mntmModificar = new JMenuItem("Modificar producto");
+		mntmListar = new JMenuItem("Listar productos");
 		mntmVender = new JMenuItem("Vender");
 		mntmConfigurarDescuentos = new JMenuItem("Configurar descuentos");
 		mntmConfigurarObsequios = new JMenuItem("Configurar obsequios");
 		mntmAcercaDeTienda = new JMenuItem("Acerca de Tienda");
+		
+		// set action events
+		mntmSalir.addActionListener(this);
+		mntmConsultar.addActionListener(this);
+		mntmModificar.addActionListener(this);
+		mntmListar.addActionListener(this);
+		mntmVender.addActionListener(this);
+		mntmConfigurarDescuentos.addActionListener(this);
+		mntmConfigurarObsequios.addActionListener(this);
+		mntmAcercaDeTienda.addActionListener(this);
 		
 		// set menus
 		mnArchivo = new JMenu("Archivo");
@@ -95,4 +108,42 @@ public class Home extends JFrame {
 		setContentPane(contentPane);
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Object source = e.getSource();
+		
+		if(source == mntmSalir) {
+			this.alertMessage("Salir");
+		}
+		if(source == mntmConsultar) {
+			this.alertMessage("Consultar producto");
+		}
+		if(source == mntmModificar) {
+			this.alertMessage("Modificar producto");
+		}
+		if(source == mntmListar) {
+			this.alertMessage("Listar productos");
+		}
+		if(source == mntmVender) {
+			this.alertMessage("Vender");		
+		}
+		if(source == mntmConfigurarDescuentos) {
+			this.alertMessage("configurar descuentos");
+		}
+		if(source == mntmAcercaDeTienda) {
+			this.alertMessage("Acerca de Tienda");
+		}
+	}
+	
+	private void alertMessage(String name) {
+		JOptionPane.showMessageDialog(
+            null,
+            "Has seleccionado: " + name,
+            name,
+            JOptionPane.INFORMATION_MESSAGE
+        );
+	}
+
 }
+
