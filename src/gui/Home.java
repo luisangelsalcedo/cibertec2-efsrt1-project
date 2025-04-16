3ffb14e2ad0e02566e475cb441a26c9437f1d005
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-04-15 20:44:34 -0500

    feat: Add JmenuItems

--- src/gui/Home.java
+++ src/gui/Home.java
@@ -7,9 +7,12 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Home extends JFrame {
 
+
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JMenuBar menuBar;
 	private JMenu mnArchivo;
@@ -17,6 +20,14 @@ public class Home extends JFrame {
 	private JMenu mnVentas;
 	private JMenu mnConfiguracion;
 	private JMenu mnAyuda;
+	private JMenuItem mntmSalir;
+	private JMenuItem mntmConsultar;
+	private JMenuItem mntmModificar;
+	private JMenuItem mntmListar;
+	private JMenuItem mntmVender;
+	private JMenuItem mntmConfigurarDescuentos;
+	private JMenuItem mntmConfigurarObsequios;
+	private JMenuItem mntmAcercaDeTienda;
 
 	/**
 	 * Launch the application.
@@ -41,26 +52,46 @@ public class Home extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		
-		menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
+		// set menu Items
+		mntmSalir = new JMenuItem("Salir");
+		mntmConsultar = new JMenuItem("Consultar");
+		mntmModificar = new JMenuItem("Modificar");
+		mntmListar = new JMenuItem("Listar");
+		mntmVender = new JMenuItem("Vender");
+		mntmConfigurarDescuentos = new JMenuItem("Configurar descuentos");
+		mntmConfigurarObsequios = new JMenuItem("Configurar obsequios");
+		mntmAcercaDeTienda = new JMenuItem("Acerca de Tienda");
 		
+		// set menus
 		mnArchivo = new JMenu("Archivo");
-		menuBar.add(mnArchivo);
+		mnArchivo.add(mntmSalir);		
 		
 		mnMantenimiento = new JMenu("Mantenimiento");
+		mnMantenimiento.add(mntmConsultar);
+		mnMantenimiento.add(mntmModificar);		
+		mnMantenimiento.add(mntmListar);
+		
+		mnVentas = new JMenu("Ventas");		
+		mnVentas.add(mntmVender);
+		
+		mnConfiguracion = new JMenu("Configuracion");		
+		mnConfiguracion.add(mntmConfigurarDescuentos);		
+		mnConfiguracion.add(mntmConfigurarObsequios);
+		
+		mnAyuda = new JMenu("Ayuda");		
+		mnAyuda.add(mntmAcercaDeTienda);
+		
+		// set menu bar
+		menuBar = new JMenuBar();
+		menuBar.add(mnArchivo);
 		menuBar.add(mnMantenimiento);
-		
-		mnVentas = new JMenu("Ventas");
 		menuBar.add(mnVentas);
-		
-		mnConfiguracion = new JMenu("Configuracion");
 		menuBar.add(mnConfiguracion);
-		
-		mnAyuda = new JMenu("Ayuda");
 		menuBar.add(mnAyuda);
+		setJMenuBar(menuBar);
+		
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));		
 		setContentPane(contentPane);
 	}
 
