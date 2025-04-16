f24512b15571033f1ed16f4075590641181e9c20
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-04-15 20:38:40 -0500

    feat: Add jmenus

--- src/gui/Home.java
+++ src/gui/Home.java
@@ -5,10 +5,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
 
 public class Home extends JFrame {
 
 	private JPanel contentPane;
+	private JMenuBar menuBar;
+	private JMenu mnArchivo;
+	private JMenu mnMantenimiento;
+	private JMenu mnVentas;
+	private JMenu mnConfiguracion;
+	private JMenu mnAyuda;
 
 	/**
 	 * Launch the application.
@@ -32,6 +40,24 @@ public class Home extends JFrame {
 	public Home() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		mnArchivo = new JMenu("Archivo");
+		menuBar.add(mnArchivo);
+		
+		mnMantenimiento = new JMenu("Mantenimiento");
+		menuBar.add(mnMantenimiento);
+		
+		mnVentas = new JMenu("Ventas");
+		menuBar.add(mnVentas);
+		
+		mnConfiguracion = new JMenu("Configuracion");
+		menuBar.add(mnConfiguracion);
+		
+		mnAyuda = new JMenu("Ayuda");
+		menuBar.add(mnAyuda);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
