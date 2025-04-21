e42eff3fe2fa90049ba4e7cb67bc472995c9abac
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-04-21 02:20:59 -0500

    feat: Add static data

---
+++ src/models/AppData.java
@@ -0,0 +1,115 @@
+package models;
+
+public class AppData {
+	
+//	product 1
+	public static String productName1 = "promo 1";
+	public static String burgerName1 = "";
+	public static String burgerSize1 = "Regular";
+	public static int burgerCount1 = 1;
+	public static String potatoesSize1 = "Regular";
+	public static int potatoesCount1 = 1;
+	public static String sodaName1 = "";
+	public static int sodaCount1 = 1;
+	public static double price1 = 0.0;
+
+//	product 2
+	public static String productName2 = "promo 2";
+	public static String burgerName2 = "";
+	public static String burgerSize2 = "Regular";
+	public static int burgerCount2 = 1;
+	public static String potatoesSize2 = "Regular";
+	public static int potatoesCount2 = 1;
+	public static String sodaName2 = "";
+	public static int sodaCount2 = 1;
+	public static double price2 = 0.0;
+	
+//	product 3
+	public static String productName3 = "promo 3";
+	public static String burgerName3 = "";
+	public static String burgerSize3 = "Regular";
+	public static int burgerCount3 = 1;
+	public static String potatoesSize3 = "Regular";
+	public static int potatoesCount3 = 1;
+	public static String sodaName3 = "";
+	public static int sodaCount3 = 1;
+	public static double price3 = 0.0;
+
+//	product 4
+	public static String productName4 = "promo 4";
+	public static String burgerName4 = "";
+	public static String burgerSize4 = "Regular";
+	public static int burgerCount4 = 1;
+	public static String potatoesSize4 = "Regular";
+	public static int potatoesCount4 = 1;
+	public static String sodaName4 = "";
+	public static int sodaCount4 = 1;
+	public static double price4 = 0.0;
+
+//	product 5
+	public static String productName5 = "promo 5";
+	public static String burgerName5 = "";
+	public static String burgerSize5 = "Regular";
+	public static int burgerCount5 = 1;
+	public static String potatoesSize5 = "Regular";
+	public static int potatoesCount5 = 1;
+	public static String sodaName5 = "";
+	public static int sodaCount5 = 1;
+	public static double price5 = 0.0;
+
+//	product 6
+	public static String productName6 = "promo 6";
+	public static String burgerName6 = "";
+	public static String burgerSize6 = "Regular";
+	public static int burgerCount6 = 1;
+	public static String potatoesSize6 = "Regular";
+	public static int potatoesCount6 = 1;
+	public static String sodaName6 = "";
+	public static int sodaCount6 = 1;
+	public static double price6 = 0.0;
+	
+	
+	public static String[] burgers = {
+			"La Épica",
+			"Extrema",
+			"Parrillera",
+			"La carretillera",
+			"Mexicana",
+			"La Churrita",
+			"A lo Pobre",
+			"Hawaiana",
+			"Royal",
+			"Queso Tocino",
+			"Cheese",
+			"Clásica"
+	};
+	
+	public static String[] burgerSlugs = {
+			"epica",
+			"extrema",
+			"parrillera",
+			"carretillera",
+			"mexicana",
+			"churrita",
+			"a-lo-pobre",
+			"hawaiana",
+			"royal",
+			"queso-tocino",
+			"cheese",
+			"clasica"
+	};
+	
+	public static String[] sizeProducts = {
+			"Regular", 
+			"Mediana", 
+			"Grande"
+	};
+	
+	public static String[] sodas = {
+			"Coca Cola",
+			"Inca kola",
+			"Fanta"
+	};
+	
+	public static String sourcePath = "C:\\Users\\seemc\\Work\\cibertec\\cibertec1-iaa-project\\assets\\";
+}
