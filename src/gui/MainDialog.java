3b604a03f59d1c659b31af26770cd8d32139d88d
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-04-22 06:00:07 -0500

    feat: Add  look and feel color to MainDialog

--- src/gui/MainDialog.java
+++ src/gui/MainDialog.java
@@ -3,17 +3,22 @@ import java.awt.BorderLayout;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 
+import models.AppData;
+import java.awt.Color;
+
 public class MainDialog extends JDialog {
 	
 	public MainDialog() {
 		setSize(450, 300);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout());
+		getContentPane().setBackground(AppData.$primaryColor);
+		setBackground(AppData.$primaryColor);
 	}
 	
 	public void showView(JPanel panel) {
 		getContentPane().removeAll();
-		add(panel);
+		getContentPane().add(panel);
 		pack();
 		setVisible(true);
 	}
