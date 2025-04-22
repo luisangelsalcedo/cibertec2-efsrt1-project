8e8d27245e6c2cb781f6921b6ee37cc7cf9d7335
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-04-22 06:01:55 -0500

    feat: Implement ViewGraphics.java

--- src/gui/ViewGraphics.java
+++ src/gui/ViewGraphics.java
@@ -1,120 +1,86 @@
 package gui;
-
 import java.awt.Font;
-
+import java.awt.Color;
+import models.AppData;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 
-import models.AppData;
-import java.awt.Color;
-
 public class ViewGraphics extends JPanel {
-	
-	JLabel burgerImage;
-	JLabel potatoeImage;
-	JLabel sodaImage;
-	JLabel ellipseImage1;
-	JLabel ellipseImage2;
-	JLabel ellipseImage3;
-	
-	JLabel burgerCount;	
-	JLabel potatoeCount;	
-	JLabel sodaCount;
-		
-	JTextPane burgerDescription;
-	JTextPane potatoeDescription;
-	JTextPane sodaDescription;
-	
-	ImageIcon burgerSrc;
-	ImageIcon potatoesSrc;
-	ImageIcon sodaSrc;
-	ImageIcon ellipseSrc;
 
-	public ViewGraphics(
-			String bImage,
-			String bName,
-			String bSize,
-			int bCount,			
-			String pSize,
-			int pCount,
-			String sImage,
-			String sName,
-			int sCount			
-		) {
+	private JLabel burgerImage;
+	private JLabel potatoeImage;
+	private JLabel sodaImage;
+	private JLabel ellipseImage1;
+	private JLabel ellipseImage2;
+	private JLabel ellipseImage3;
+	private JLabel burgerCount;
+	private JLabel potatoeCount;
+	private JLabel sodaCount;
+	private JTextPane burgerDescription;
+	private JTextPane potatoeDescription;
+	private JTextPane sodaDescription;
+	private ImageIcon burgerImagePath;
+	private ImageIcon potatoesImagePath;
+	private ImageIcon sodaImagePath;
+	private ImageIcon ellipseSrc;
+
+	private String sourcePath = AppData.sourcePath;
+
+
+	public ViewGraphics() {
 		setBackground(new Color(255, 0, 0));
-		
-		String sourcePath = AppData.sourcePath;
-		
-		String bbCount = bCount == 0 ? "" : "" + bCount;
-		String ppCount = pCount == 0 ? "" : "" + pCount;
-		String ssCount = sCount == 0 ? "" : "" + sCount;
-		
 
-		burgerSrc = bImage == null ? null : new ImageIcon(sourcePath + bImage);
-		sodaSrc = sImage == null ? null : new ImageIcon(sourcePath + sImage);
-		potatoesSrc = new ImageIcon(sourcePath + "papitas.png");
-		ellipseSrc = new ImageIcon(sourcePath + "ellipse.png");
-
-		
 		burgerImage = new JLabel();
 		burgerImage.setBounds(22, 27, 150, 150);
-		burgerImage.setIcon(burgerSrc);
 		burgerDescription = new JTextPane();
-		burgerDescription.setForeground(new Color(255, 255, 255));
+		burgerDescription.setForeground(AppData.$white);
 		burgerDescription.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		burgerDescription.setEnabled(false);
 		burgerDescription.setEditable(false);
 		burgerDescription.setOpaque(false);
-		burgerDescription.setBounds(58, 159, 113, 36);
-		burgerDescription.setText(bName + "\n" + bSize);
+		burgerDescription.setBounds(58, 159, 113, 36);		
 		burgerCount = new JLabel();
-		burgerCount.setText(bbCount);
-		burgerCount.setForeground(new Color(255, 255, 255));
+		burgerCount.setForeground(AppData.$white);
 		burgerCount.setBounds(33, 160, 35, 35);
 		burgerCount.setFont(new Font("Tahoma", Font.BOLD, 20));
 
-		
 		potatoeImage = new JLabel();
 		potatoeImage.setBounds(150, 17, 150, 150);
-		potatoeImage.setIcon(potatoesSrc);
 		potatoeDescription = new JTextPane();
-		potatoeDescription.setForeground(new Color(255, 255, 255));
+		potatoeDescription.setForeground(AppData.$white);
 		potatoeDescription.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		potatoeDescription.setEnabled(false);
 		potatoeDescription.setEditable(false);
 		potatoeDescription.setOpaque(false);
 		potatoeDescription.setBounds(215, 166, 74, 23);
-		potatoeDescription.setText(pSize);
 		potatoeCount = new JLabel();
-		potatoeCount.setText(ppCount);
-		potatoeCount.setForeground(new Color(255, 255, 255));
+		potatoeCount.setForeground(AppData.$white);
 		potatoeCount.setBounds(191, 160, 35, 35);
 		potatoeCount.setFont(new Font("Tahoma", Font.BOLD, 20));
 
-		
 		sodaImage = new JLabel();
 		sodaImage.setBounds(276, 27, 150, 150);
-		sodaImage.setIcon(sodaSrc);
 		sodaDescription = new JTextPane();
-		sodaDescription.setForeground(new Color(255, 255, 255));
+		sodaDescription.setForeground(AppData.$white);
 		sodaDescription.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		sodaDescription.setEnabled(false);
 		sodaDescription.setEditable(false);
 		sodaDescription.setOpaque(false);
 		sodaDescription.setBounds(328, 166, 69, 23);
-		sodaDescription.setText(sName);
 		sodaCount = new JLabel();
-		sodaCount.setText(ssCount);
-		sodaCount.setForeground(new Color(255, 255, 255));
+		sodaCount.setForeground(AppData.$white);
 		sodaCount.setBounds(306, 160, 35, 35);
 		sodaCount.setFont(new Font("Tahoma", Font.BOLD, 20));
+
+		ellipseSrc = new ImageIcon(sourcePath + "ellipse.png");
 		
 		ellipseImage1 = new JLabel();
 		ellipseImage1.setBounds(44, 37, 112, 112);
 		ellipseImage1.setIcon(ellipseSrc);
-		
+		ellipseImage1.setVisible(false);
+
 		ellipseImage2 = new JLabel();
 		ellipseImage2.setBounds(170, 37, 112, 112);
 		ellipseImage2.setIcon(ellipseSrc);
@@ -122,10 +88,11 @@ public class ViewGraphics extends JPanel {
 		ellipseImage3 = new JLabel();
 		ellipseImage3.setBounds(294, 37, 112, 112);
 		ellipseImage3.setIcon(ellipseSrc);
-		
+		ellipseImage3.setVisible(false);
+
 		setLayout(null);
 		setBackground(new Color(2, 22, 137));
-		add(burgerCount);		
+		add(burgerCount);
 		add(potatoeCount);
 		add(sodaCount);
 		add(burgerDescription);
@@ -137,6 +104,59 @@ public class ViewGraphics extends JPanel {
 		add(ellipseImage1);
 		add(ellipseImage2);
 		add(ellipseImage3);
+	}
+
+	public void setBurger(int count, String image, String name, String size) {
+		
+		if(count > 0) {			
+			burgerImagePath = new ImageIcon(sourcePath + image);
+			burgerImage.setIcon(burgerImagePath);
+			burgerCount.setText(String.valueOf(count));		
+			burgerDescription.setText(name + "\n" + size);
+		} else this.cleanBurger();
+	}
+
+	public void setPotatoe(int count, String size) {
+		
+		if(count > 0) {	
+			potatoesImagePath = new ImageIcon(sourcePath + "papitas.png");
+			potatoeImage.setIcon(potatoesImagePath);
+			potatoeDescription.setText(size);
+			potatoeCount.setText(String.valueOf(count));
+		} else this.cleanPotatoes();
+	}
+
+	public void setSoda(int count, String image, String name) {
+		
+		if(count > 0) {	
+			sodaImagePath = new ImageIcon(sourcePath + image);
+			sodaImage.setIcon(sodaImagePath);
+			sodaCount.setText(String.valueOf(count));
+			sodaDescription.setText(name);
+		} else this.cleanSoda();
+	}
+	
+	public void clean() {
+		this.cleanBurger();
+		this.cleanPotatoes();
+		this.cleanSoda();
+	}
+	
+	public void cleanBurger() {
+		burgerCount.setText(null);
+		burgerImage.setIcon(null);
+		burgerDescription.setText(null);		
+	}
+	public void cleanPotatoes() {		
+		potatoeDescription.setText(null);
+		potatoeImage.setIcon(null);
+		potatoeCount.setText(null);
+
+	}
+	public void cleanSoda() {
+		sodaCount.setText(null);
+		sodaDescription.setText(null);
+		sodaImage.setIcon(null);
 	}
 
 }
