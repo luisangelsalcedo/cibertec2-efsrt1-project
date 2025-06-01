f57899deee4afdee315af5dd45d202ee8feee784
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-06-01 14:59:55 -0500

    refactor: Separate main menu into its own class

--- src/bembos/views/Home.java
+++ src/bembos/views/Home.java
@@ -3,34 +3,22 @@ package bembos.views;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import bembos.views.components.MainDialog;
+import bembos.views.components.MainMenu;
 import db.AppData;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 import java.awt.Toolkit;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 
-public class Home extends JFrame implements ActionListener {
+
+public class Home extends JFrame {
 
 	private static final long serialVersionUID = 1L;	
-	private JMenuItem mntmSalir;
-	private JMenuItem mntmConsultar;
-	private JMenuItem mntmModificar;
-	private JMenuItem mntmListar;
-	private JMenuItem mntmVender;
-	private JMenuItem mntmConfigurarDescuentos;
-	private JMenuItem mntmConfigurarObsequios;
-	private JMenuItem mntmAcercaDeTienda;	
 	private URL favicon = getClass().getResource(AppData.sourcePath + AppData.favicon);
 
 	/**
@@ -39,83 +27,16 @@ public class Home extends JFrame implements ActionListener {
 	public Home() {		
 		
 		setTitle(AppData.AppTitle);
-		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		setSize(500, 360);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(650, 450);
 		setLocationRelativeTo(null);
 		setIconImage(
 			Toolkit.getDefaultToolkit().getImage(favicon)
 		);
 		
-		Font fontMenu = new Font("Dialog", Font.BOLD, 14);
-		
-		// set menu Items
-		mntmSalir 					= new JMenuItem("Salir");
-		mntmConsultar 				= new JMenuItem("Consultar producto");
-		mntmModificar 				= new JMenuItem("Modificar producto");
-		mntmListar 					= new JMenuItem("Listar productos");
-		mntmVender 					= new JMenuItem("Vender");
-		mntmConfigurarDescuentos 	= new JMenuItem("Configurar descuentos");
-		mntmConfigurarObsequios 	= new JMenuItem("Configurar obsequios");
-		mntmAcercaDeTienda 			= new JMenuItem("Acerca de la aplicación");
-		
-		// set submenu font
-		mntmSalir.setFont(fontMenu);
-		mntmConsultar.setFont(fontMenu);
-		mntmModificar.setFont(fontMenu);
-		mntmListar.setFont(fontMenu);
-		mntmVender.setFont(fontMenu);
-		mntmConfigurarDescuentos.setFont(fontMenu);
-		mntmConfigurarObsequios.setFont(fontMenu);
-		mntmAcercaDeTienda.setFont(fontMenu);
-		
-		// set action events
-		mntmSalir.addActionListener(this);
-		mntmConsultar.addActionListener(this);
-		mntmModificar.addActionListener(this);
-		mntmListar.addActionListener(this);
-		mntmVender.addActionListener(this);
-		mntmConfigurarDescuentos.addActionListener(this);
-		mntmConfigurarObsequios.addActionListener(this);
-		mntmAcercaDeTienda.addActionListener(this);
-		
-		// set menus
-		JMenu mnArchivo = new JMenu("Archivo");
-		mnArchivo.setFont(fontMenu);
-		mnArchivo.setForeground(AppData.$primaryColor);
-		mnArchivo.add(mntmSalir);		
-		
-		JMenu mnMantenimiento = new JMenu("Mantenimiento");
-		mnMantenimiento.setFont(fontMenu);
-		mnMantenimiento.setForeground(AppData.$primaryColor);
-		mnMantenimiento.add(mntmConsultar);
-		mnMantenimiento.add(mntmModificar);		
-		mnMantenimiento.add(mntmListar);
-		
-		JMenu mnVentas = new JMenu("Ventas");
-		mnVentas.setFont(fontMenu);
-		mnVentas.setForeground(AppData.$primaryColor);
-		mnVentas.add(mntmVender);
-		
-		JMenu mnConfiguracion = new JMenu("Configuración");
-		mnConfiguracion.setFont(fontMenu);
-		mnConfiguracion.setForeground(AppData.$primaryColor);
-		mnConfiguracion.add(mntmConfigurarDescuentos);		
-		mnConfiguracion.add(mntmConfigurarObsequios);
-		
-		JMenu mnAyuda = new JMenu("Ayuda");
-		mnAyuda.setFont(fontMenu);
-		mnAyuda.setForeground(AppData.$primaryColor);
-		mnAyuda.add(mntmAcercaDeTienda);
-		
-		// set menu bar
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBackground(AppData.$secondaryColor);
-		menuBar.add(mnArchivo);
-		menuBar.add(mnMantenimiento);
-		menuBar.add(mnVentas);
-		menuBar.add(mnConfiguracion);
-		menuBar.add(mnAyuda);
-		setJMenuBar(menuBar);
+		// menu
+		MainMenu menuBar = new MainMenu();
+		setJMenuBar(menuBar.getComponent());
 		
 		// disclaimer
 		JLabel lblLogo = new JLabel();
@@ -163,56 +84,6 @@ public class Home extends JFrame implements ActionListener {
 		setContentPane(contentPane);
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub  
-		Object source = e.getSource();
-		
-		MainDialog dialog = new MainDialog();
-		dialog.setIconImage(
-			Toolkit.getDefaultToolkit().getImage(favicon)
-		);
-		
-		if(source == mntmSalir) {
-			System.exit(0);
-		}
-		if(source == mntmConsultar) {
-			ViewGetProduct getProductPanel = new ViewGetProduct(dialog);
-			dialog.setTitle("Consultar");
-			dialog.showView(getProductPanel);
-		}
-		if(source == mntmModificar) {
-			ViewSetProduct setProductPanel = new ViewSetProduct(dialog);
-			dialog.setTitle("Modificar");
-			dialog.showView(setProductPanel);
-		}
-		if(source == mntmListar) {
-			ViewGetAllProducts getAllProductsPanel = new ViewGetAllProducts(dialog);
-			dialog.setTitle("Listar");
-			dialog.showView(getAllProductsPanel);
-		}
-		if(source == mntmVender) {
-			ViewSellProduct sellProductPanel = new ViewSellProduct(dialog);
-			dialog.setTitle("Vender");
-			dialog.showView(sellProductPanel);
-		}
-		if(source == mntmConfigurarDescuentos) {
-			ViewDiscountsConfiguration discountsConfigurationPanel = new ViewDiscountsConfiguration(dialog);
-			dialog.setTitle("Configurar Descuentos");
-			dialog.showView(discountsConfigurationPanel);
-		}
-		if(source == mntmConfigurarObsequios) {			
-			ViewGiftsConfiguration giftsConfigurationPanel = new ViewGiftsConfiguration(dialog);
-			dialog.setTitle("Configurar Obsequios");
-			dialog.showView(giftsConfigurationPanel);
-		}
-		if(source == mntmAcercaDeTienda) {
-			ViewAboutApp aboutAppPanel = new ViewAboutApp();
-			dialog.setTitle("Acerca de la Aplicacion");
-			dialog.showView(aboutAppPanel);
-		}
 	
-		dialog.setLocationRelativeTo(this);
-	}
 }
 
---
+++ src/bembos/views/components/MainMenu.java
@@ -0,0 +1,179 @@
+package bembos.views.components;
+
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import bembos.views.ViewAboutApp;
+import bembos.views.ViewDiscountsConfiguration;
+import bembos.views.ViewGetAllProducts;
+import bembos.views.ViewGetProduct;
+import bembos.views.ViewGiftsConfiguration;
+import bembos.views.ViewSellProduct;
+import bembos.views.ViewSetProduct;
+import db.AppData;
+
+public class MainMenu extends JMenuBar implements ActionListener {
+	
+	private static final long serialVersionUID = 1L;
+	private JMenuItem mntmSalir;
+	private JMenuItem mntmConsultar;
+	private JMenuItem mntmModificar;
+	private JMenuItem mntmListar;
+	private JMenuItem mntmVender;
+	private JMenuItem mntmConfigurarDescuentos;
+	private JMenuItem mntmConfigurarObsequios;
+	private JMenuItem mntmAcercaDeTienda;	
+	private JMenuItem mntmVerUsuario;	
+	private JMenuItem mntmListarUsuarios;	
+	private JMenuItem mntmAgregarUsuario;	
+	private JMenuBar menuBar;
+	private URL favicon = getClass().getResource(AppData.sourcePath + AppData.favicon);
+	
+	public MainMenu(){
+		Font fontMenu = new Font("Dialog", Font.BOLD, 15);
+		Font fontSubMenu = new Font("Dialog", Font.BOLD, 15);
+		
+		// set menu Items
+		mntmSalir 					= new JMenuItem("Salir");
+		mntmConsultar 				= new JMenuItem("Consultar producto");
+		mntmModificar 				= new JMenuItem("Modificar producto");
+		mntmListar 					= new JMenuItem("Listar productos");
+		mntmVender 					= new JMenuItem("Vender");
+		mntmConfigurarDescuentos 	= new JMenuItem("Configurar descuentos");
+		mntmConfigurarObsequios 	= new JMenuItem("Configurar obsequios");
+		mntmAcercaDeTienda 			= new JMenuItem("Acerca de la aplicación");
+		mntmVerUsuario	 			= new JMenuItem("Ver Usuario");
+		mntmListarUsuarios	 		= new JMenuItem("Listar Usuarios");
+		mntmAgregarUsuario	 		= new JMenuItem("Agregar Usuarios");
+		
+		// set submenu font
+		mntmSalir.setFont(fontSubMenu);
+		mntmConsultar.setFont(fontSubMenu);
+		mntmModificar.setFont(fontSubMenu);
+		mntmListar.setFont(fontSubMenu);
+		mntmVender.setFont(fontSubMenu);
+		mntmConfigurarDescuentos.setFont(fontSubMenu);
+		mntmConfigurarObsequios.setFont(fontSubMenu);
+		mntmAcercaDeTienda.setFont(fontSubMenu);
+		mntmVerUsuario.setFont(fontSubMenu);
+		mntmListarUsuarios.setFont(fontSubMenu);
+		mntmAgregarUsuario.setFont(fontSubMenu);
+		
+		// set action events
+		mntmSalir.addActionListener(this);
+		mntmConsultar.addActionListener(this);
+		mntmModificar.addActionListener(this);
+		mntmListar.addActionListener(this);
+		mntmVender.addActionListener(this);
+		mntmConfigurarDescuentos.addActionListener(this);
+		mntmConfigurarObsequios.addActionListener(this);
+		mntmAcercaDeTienda.addActionListener(this);
+		
+		// set menus
+		JMenu mnArchivo = new JMenu("Archivo");
+		mnArchivo.setFont(fontMenu);
+		mnArchivo.setForeground(AppData.$primaryColor);
+		mnArchivo.add(mntmSalir);		
+		
+		JMenu mnMantenimiento = new JMenu("Mantenimiento");
+		mnMantenimiento.setFont(fontMenu);
+		mnMantenimiento.setForeground(AppData.$primaryColor);
+		mnMantenimiento.add(mntmConsultar);
+		mnMantenimiento.add(mntmModificar);		
+		mnMantenimiento.add(mntmListar);
+		
+		JMenu mnVentas = new JMenu("Ventas");
+		mnVentas.setFont(fontMenu);
+		mnVentas.setForeground(AppData.$primaryColor);
+		mnVentas.add(mntmVender);
+		
+		JMenu mnConfiguracion = new JMenu("Configuración");
+		mnConfiguracion.setFont(fontMenu);
+		mnConfiguracion.setForeground(AppData.$primaryColor);
+		mnConfiguracion.add(mntmConfigurarDescuentos);		
+		mnConfiguracion.add(mntmConfigurarObsequios);
+		
+		JMenu mnAyuda = new JMenu("Ayuda");
+		mnAyuda.setFont(fontMenu);
+		mnAyuda.setForeground(AppData.$primaryColor);
+		mnAyuda.add(mntmAcercaDeTienda);
+		
+		JMenu mnUsuarios = new JMenu("Usuarios");
+		mnUsuarios.setFont(fontMenu);
+		mnUsuarios.setForeground(AppData.$primaryColor);
+		mnUsuarios.add(mntmVerUsuario);
+		mnUsuarios.add(mntmListarUsuarios);
+		mnUsuarios.add(mntmAgregarUsuario);
+		
+		// set menu bar
+		menuBar = new JMenuBar();
+		menuBar.setBackground(AppData.$secondaryColor);
+		menuBar.add(mnArchivo);
+		menuBar.add(mnMantenimiento);
+		menuBar.add(mnVentas);
+		menuBar.add(mnConfiguracion);
+		menuBar.add(mnAyuda);
+		menuBar.add(mnUsuarios);
+	}
+	
+	public JMenuBar getComponent() {
+		return menuBar;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub  
+		Object source = e.getSource();
+		
+		MainDialog dialog = new MainDialog();
+		dialog.setIconImage(
+			Toolkit.getDefaultToolkit().getImage(favicon)
+		);
+		
+		if(source == mntmSalir) {
+			System.exit(0);
+		}
+		if(source == mntmConsultar) {
+			ViewGetProduct getProductPanel = new ViewGetProduct(dialog);
+			dialog.setTitle("Consultar");
+			dialog.showView(getProductPanel);
+		}
+		if(source == mntmModificar) {
+			ViewSetProduct setProductPanel = new ViewSetProduct(dialog);
+			dialog.setTitle("Modificar");
+			dialog.showView(setProductPanel);
+		}
+		if(source == mntmListar) {
+			ViewGetAllProducts getAllProductsPanel = new ViewGetAllProducts(dialog);
+			dialog.setTitle("Listar");
+			dialog.showView(getAllProductsPanel);
+		}
+		if(source == mntmVender) {
+			ViewSellProduct sellProductPanel = new ViewSellProduct(dialog);
+			dialog.setTitle("Vender");
+			dialog.showView(sellProductPanel);
+		}
+		if(source == mntmConfigurarDescuentos) {
+			ViewDiscountsConfiguration discountsConfigurationPanel = new ViewDiscountsConfiguration(dialog);
+			dialog.setTitle("Configurar Descuentos");
+			dialog.showView(discountsConfigurationPanel);
+		}
+		if(source == mntmConfigurarObsequios) {			
+			ViewGiftsConfiguration giftsConfigurationPanel = new ViewGiftsConfiguration(dialog);
+			dialog.setTitle("Configurar Obsequios");
+			dialog.showView(giftsConfigurationPanel);
+		}
+		if(source == mntmAcercaDeTienda) {
+			ViewAboutApp aboutAppPanel = new ViewAboutApp();
+			dialog.setTitle("Acerca de la Aplicacion");
+			dialog.showView(aboutAppPanel);
+		}
+	
+		dialog.setLocationRelativeTo(this);
+	}
+}
