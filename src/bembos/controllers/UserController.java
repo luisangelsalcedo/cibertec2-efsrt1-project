02cb3f1379cff73d3b641af3784c31975e53d227
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-06-03 16:17:45 -0500

    feat: Create user controller with its login method

---
+++ src/bembos/controllers/UserController.java
@@ -0,0 +1,30 @@
+package bembos.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import bembos.models.User;
+import db.AppData;
+
+public class UserController {
+	private List<User> userList = new ArrayList<>(AppData.usersList);
+	
+	public List<User> getAllUsers(){
+		return userList;
+	}
+	
+	public boolean login(String userName, String password) {
+		System.out.print("\nuser " + userName);
+		System.out.print("\npass " + password);
+		for(User user:userList) {
+			if(
+				user.getUserName().trim().equals(userName) && 
+				user.getPassword().trim().equals(password)
+			){
+				AppData.loggedUser = user;
+				return true;
+			} 		
+		}
+		return false;
+	}
+	
+}
